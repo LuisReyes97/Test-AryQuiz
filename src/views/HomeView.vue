074381986de62e2b84b49d8e7533a31c1b32754c
Luis Reyes <iscluisreyes@gmail.com>
2022-08-18 11:12:27 -0500

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/reporte-de-salud.png">
+    <HelloWorld msg="Arry Quiz"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
